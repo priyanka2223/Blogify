7e5d4a2f08f02cdbfc8768dcc35af8df7f8ae59e
priyanka2223 <39003754+priyanka2223@users.noreply.github.com>
2024-07-28 14:51:40 +0530

    Update authentication.js

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,20 +1,14 @@
 const { validateToken } = require("../services/authentication");
 
 function checkForAuthenticationCookie(cookieName) {
-    console.log("checkForAuthenticationCookie")
-    console.log(cookieName)
     return (req, res, next) => {
         const tokenCookieValue = req.cookies[cookieName]
-        console.log("tokencookievalue")
-        console.log(tokenCookieValue)
         if (!tokenCookieValue) {
             return next()
         }
 
         try {
             const userpayload = validateToken(tokenCookieValue)
-            console.log("userpayload")
-            console.log(userpayload)
             req.user = userpayload
         }
         catch (error) { 
